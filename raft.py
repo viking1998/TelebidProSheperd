bb8b7c5b8df0b8ee9003475ef7d157dd18ee3e27
petar <google1999@abv.bg>
2021-04-05 19:07:46 +0300

    Made solution iterative

--- raft.py
+++ raft.py
@@ -1,25 +1,15 @@
-def check_capacity(capacity, weights, weights_used, trips_left, trips):
-    if trips_left >= 0 and len(weights) == len(weights_used):
-        print(capacity)
-        return capacity
-    if trips_left == 0 and len(weights) > len(weights_used):
-        return 0
-    current_weight_index = 0
-    trip_capacity = capacity
-
-    while current_weight_index < len(weights) and trip_capacity > 0:
-        if current_weight_index not in weights_used and trip_capacity - weights[current_weight_index] >= 0:
-            trip_capacity -= weights[current_weight_index]
-            weights_used.append(current_weight_index)
-        current_weight_index += 1
-
-    if check_capacity(capacity, weights, weights_used, trips_left - 1, trips) == 0:
-        if trips_left == trips:
-            return check_capacity(capacity + 1, weights, [], trips, trips)
-        else:
-            return 0
-    else:
-        return capacity
+def check_capacity(capacity, weights, weights_used, trips):
+    for i in range(trips):
+        temp_capacity = capacity
+        current_weight_index = 0
+        while current_weight_index < len(weights) and temp_capacity > 0:
+            if current_weight_index not in weights_used and temp_capacity - weights[current_weight_index] >= 0:
+                temp_capacity -= weights[current_weight_index]
+                weights_used.append(current_weight_index)
+            current_weight_index += 1
+        if len(weights) == len(weights_used):
+            return True
+    return False
 
 
 first_input_line_split = input().split(" ")
@@ -30,4 +20,8 @@ goat_weights = list(map(int, input().split(" ")))
 goat_weights.sort(reverse=True)
 min_capacity = goat_weights[0]
 
-check_capacity(min_capacity, goat_weights, [], max_trips, max_trips)
+while True:
+    if check_capacity(min_capacity, goat_weights, [], max_trips):
+        print(min_capacity)
+        break
+    min_capacity += 1
